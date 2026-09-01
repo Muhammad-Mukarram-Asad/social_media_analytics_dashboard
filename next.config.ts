3aae44a9f319acdd8c00933495f7954208fd0441
Mukarram Asad <76425569+Muhammad-Mukarram-Asad@users.noreply.github.com>
2026-09-02 00:00:26 +0500

    Replace showNexticon with devIndicators option

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  showNexticon: false
+  devIndicators: false,
 };
 
 export default nextConfig;
